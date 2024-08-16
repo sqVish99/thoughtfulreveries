51a53e2fe18609d8181301d3c82ed3d193f11303
sqVish99 <vmharish@asu.edu>
2024-08-17 01:51:54 +0530

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Home from './components/pages/home/Home';
