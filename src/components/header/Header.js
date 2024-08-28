1e7e015308a721114e8797b690493d4646891425
sqVish99 <vmharish@asu.edu>
2024-08-28 14:09:50 -0700

    cosmetic changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './Header.css';
+import LeftArrow from '../../assets/icons/left-arrow.svg';
 
 const Header = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleClick = () => {
     navigate('/');
   };
 
+  const handleBackClick = () => {
+    navigate('/');
+  };
+
   return (
     <header>
+      {location.pathname !== '/' && (
+        <div className="back-button" onClick={handleBackClick}>
+          <img src={LeftArrow} className="back-arrow-icon" />
+        </div>
+      )}
       <h1 className='header-content' onClick={handleClick}>Thoughtful Reveries</h1>
     </header>
   );
